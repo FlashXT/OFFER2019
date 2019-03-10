1ea757c98d959c3879ac96246c1e44975eb639ac
turboMan <turboman2017@qq.com>
2019-03-10 19:15:11 +0800

    QuickSort complete,2019.3.10,Sunday,FlashXT.

--- src/SortAlgorithm/SortAlgorithm.java
+++ src/SortAlgorithm/SortAlgorithm.java
@@ -2,13 +2,19 @@ package SortAlgorithm;
 import java.util.*;
 public class SortAlgorithm {
     public static void main(String[]args){
-        int [] arr = {12,34,56,45,67,45,89,30};
+        int [] arr = {12,10,38,42,75,68,54,9,40};
 
 //        System.out.println(Arrays.toString(BubbleSort(arr)));
 //        System.out.println(Arrays.toString(InsertSort(arr)));
 //        System.out.println(Arrays.toString(SelectSort(arr)));
 //        MergeSort(arr,0,arr.length-1);
 //        System.out.println(Arrays.toString(arr));
+        Quicksort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+//        int index = partitionBorder(arr,0,arr.length-1);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(index);
+
     }
 
     //swap
@@ -89,6 +95,51 @@ public class SortAlgorithm {
             arr[start + i] = temp[i];
     }
 
+    //快速排序
+    public static void Quicksort(int [] arr,int start, int end){
+        if(start >= end) return ;
+//        int pivot = partitionBorder(arr,start,end);
+        int pivot = partition(arr,start,end);
+        Quicksort(arr,start,pivot-1);
+        Quicksort(arr,pivot+1,end);
+
+    }
+
+    public static int partitionBorder(int [] arr,int start,int end){
+
+        int pivot = arr[end];               //初始枢轴选择输入数组的最后一个元素
+        int lessBorder = start - 1;         //初始小于pivot的数字的边界在0元素之前；
+        int i = start;
+        while(i<= end){
+            if(arr[i] <= pivot){  //“=”是将枢轴元素移动到lessBroder处,返回正确的lessBorder；
+                //遍历数组将小于等于枢轴元素的值与边界的位置的值进行交换，边界向右增1；
+
+                swap(arr,++lessBorder,i);
+
+            }
+            i++;
+        }
+        return lessBorder;
+    }
+
+    public static int partition(int [] arr,int start,int end){
+        int pivot = start;
+        int low = start;
+        int high = end;
+        while(low < high){
+            if(arr[low] > arr[pivot]){
+                swap(arr,low,pivot);
+                pivot = low;
+            }
+            if(arr[high] < arr[pivot]){
+                swap(arr,high,pivot);
+                pivot = high;
+            }
+            low++;
+            high--;
+        }
+        return pivot;
+    }
 
 
 
