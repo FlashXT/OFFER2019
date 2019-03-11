166d0958b16049618c4d449bd576c3bdc256b748
turboMan <turboman2017@qq.com>
2019-03-11 10:58:51 +0800

    FlagOfNetherland,complete,2019.3.10,Sunday,FlashXT.

---
+++ src/SortAlgorithm/FlagOfNetherland.java
@@ -0,0 +1,52 @@
+package SortAlgorithm;
+
+import java.util.Arrays;
+
+public class FlagOfNetherland {
+    public static void main(String [] args){
+
+        int [] arr = {1,2,4,38,34,4,4,4,75,54,9,4};
+        Quicksort(arr,0, arr.length-1);
+    }
+
+    //swapint [] arr = {12,10,38,42,75,68,54,9,40};
+    public static void swap(int [] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    //快速排序
+    public static void Quicksort(int [] arr,int start, int end){
+        if(start >= end) return ;
+        int []  border = partitionBorder(arr,start,end);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(border));
+
+    }
+
+    public static int[] partitionBorder(int [] arr,int start,int end){
+
+        int pivot = arr[end];               //初始枢轴选择输入数组的最后一个元素
+        int lessBorder = start - 1;         //初始小于pivot的数字的边界在0元素之前；
+        int moreBorder = end + 1;
+        int i = start;
+        while(i < moreBorder){
+            if(arr[i] < pivot){
+
+                swap(arr,++lessBorder,i);
+                i++;
+
+            }
+            else if(arr[i] == pivot){
+                i++;
+            }
+            else{
+                swap(arr,--moreBorder,i);
+            }
+
+        }
+        
+        return new int[]{lessBorder,moreBorder};
+    }
+}
