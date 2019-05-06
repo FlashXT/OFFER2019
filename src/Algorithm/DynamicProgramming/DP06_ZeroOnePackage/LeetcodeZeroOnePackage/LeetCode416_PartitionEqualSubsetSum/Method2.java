e57810870149595d1ddd9d5bf10e12f8e9df97e4
FlashXT <976997551@qq.com>
2019-05-06 18:24:51 +0800

    DP_Day06,2019.5.6，FlashXT.Fighting！

---
+++ src/Algorithm/DynamicProgramming/DP06_ZeroOnePackage/LeetcodeZeroOnePackage/LeetCode416_PartitionEqualSubsetSum/Method2.java
@@ -0,0 +1,26 @@
+package Algorithm.DynamicProgramming.DP06_ZeroOnePackage.LeetcodeZeroOnePackage.LeetCode416_PartitionEqualSubsetSum;
+
+public class Method2 {
+    //动态规划
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i< nums.length;i++){
+            sum+=nums[i];
+        }
+        if(sum%2 != 0) return false;
+        int C = sum/2;
+        boolean[] memo = new boolean[C+1];
+
+        for(int j = 0; j < C+1;j++)
+            memo[j] = nums[0]==j;
+        for(int i = 1; i< nums.length;i++){
+            for(int j = C; j >= nums[i];j--) {
+                memo[j] = memo[j]||memo[j - nums[i]];
+            }
+
+        }
+        return memo[C];
+
+    }
+
+}
---
+++ src/Algorithm/DynamicProgramming/DP06_ZeroOnePackage/LeetcodeZeroOnePackage/LeetCode416_PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
@@ -0,0 +1,13 @@
+package Algorithm.DynamicProgramming.DP06_ZeroOnePackage.LeetcodeZeroOnePackage.LeetCode416_PartitionEqualSubsetSum;
+
+import java.util.TreeMap;
+
+public class PartitionEqualSubsetSum {
+    public static void main (String [] args){
+        int [] nums = {1,2,5};
+        System.out.println(Method1.canPartition(nums));
+        System.out.println(Method2.canPartition(nums));
+
+    }
+
+}
