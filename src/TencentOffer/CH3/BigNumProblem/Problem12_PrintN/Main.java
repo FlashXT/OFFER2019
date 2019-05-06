9e452acbd8772b0f4e6ac3217f296feb7cac9eac
turboMan <turboman2017@qq.com>
2019-05-06 22:33:33 +0800

    TencentOffer_Day04,2019.5.6,turboMan.

---
+++ src/TencentOffer/CH3/BigNumProblem/Problem12_PrintN/Main.java
@@ -0,0 +1,11 @@
+package TencentOffer.CH3.BigNumProblem.Problem12_PrintN;
+
+public class Main {
+    public static void main(String [] args){
+
+//        Method1.Print1ToMaxOfDigits_1(30);
+        Method3.Print1ToMaxOfDigits_3(3);
+    }
+
+
+}
---
+++ src/TencentOffer/CH3/BigNumProblem/Problem12_PrintN/Method2.java
@@ -0,0 +1,60 @@
+package TencentOffer.CH3.BigNumProblem.Problem12_PrintN;
+
+import java.util.Arrays;
+
+public class Method2 {
+
+    //对于大数问题，通常使用字符串或数组来表示大数,然后模拟数值运算
+    public static boolean Print1ToMaxOfDigits_2(int N) {
+        if(N <= 0){
+            System.out.println("The N is less than 1.");
+            return false;
+        }
+        char [] number = new char[N];
+        for(int i = 0; i< number.length;i++)
+            number[i] = '0';
+        while(!Increment(number)){
+            PrintNumber(number);
+        }
+        return true;
+
+    }
+    //字符串模拟整数加法
+    private static boolean Increment(char [] ch){
+        boolean isOverflow = false;
+        int nTakeOver = 0;  //代表进位
+        for(int bit = ch.length-1; bit >= 0;bit--){
+            int nSum = ch[bit]-'0'+nTakeOver;
+            if(bit == ch.length-1)
+                nSum++;     //最后一为数字做加法
+            if(nSum >= 10){ //执行进位操作
+                if(bit == 0) //进位溢出
+                    isOverflow = true;
+                else{
+                    nSum -= 10;   //进位
+                    nTakeOver  =1;
+                    ch[bit] = (char)('0'+nSum);//当前位置为仅进位后剩下的值相加
+                }
+            }
+            else{
+                ch[bit] = (char)('0'+nSum);
+                break;
+            }
+        }
+        return isOverflow;
+
+    }
+    private static void PrintNumber(char [] ch){
+
+        int i = 0;
+        for(i = 0; i < ch.length;i++){
+            if(ch[i] != '0'){
+                break;
+            }
+        }
+        while(i < ch.length){
+            System.out.printf("%c",ch[i++]);
+        }
+        System.out.println();
+    }
+}
