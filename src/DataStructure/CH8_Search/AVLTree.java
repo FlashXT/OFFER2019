cdc70c90aaabc045c2b0faa28fa506597506aa64
FlashXT <976997551@qq.com>
2019-05-10 18:05:11 +0800

    TencentOffer,Day_09,2019.5.10，FlashXT.Fighting！

--- src/DataStructure/CH8_Search/AVLTree.java
+++ src/DataStructure/CH8_Search/AVLTree.java
@@ -14,8 +14,80 @@ package DataStructure.CH8_Search;
  *    ④对P结点的右儿子的右子树进行一次插入       <--- 左旋
  *
  ************************************************************************/
+
+class AVLTreeNode<T>{
+    public T item;
+    public int bf;
+    public AVLTreeNode leftnode;
+    public AVLTreeNode rightnode;
+    public AVLTreeNode(T item){
+        this.item = item;
+        this.bf = 0;
+        this.leftnode = null;
+        this.rightnode = null;
+
+    }
+}
 public class AVLTree {
     public static void main(String [] args){
+        int [] arr = {4,1,2,3,-1,5,9,6,7,12,11};
+        AVLTreeNode<Integer> root = bulidAVLTree(arr);
+        midTraverse(root);
+        System.out.println();
+
         System.out.println("AAAAA");
+    }
+    public static AVLTreeNode<Integer> bulidAVLTree(int [] arr){
+        AVLTreeNode root= new AVLTreeNode(arr[0]);
+        for(int i = 0; i < arr.length;i++)
+            insertAVLTree(root,arr[i]);
+        return root;
+
+    }
+    public static AVLTreeNode<Integer> insertAVLTree(AVLTreeNode<Integer> root,int value){
+        if(root == null)
+            return new AVLTreeNode<>(value);
+
+        else{
+            if(value < root.item){
+                root.bf+=1;
+                root.leftnode = insertAVLTree(root.leftnode,value);
+                //左边
+                if(root.bf > 1){
+
+                }
+                else if(root.bf < -1){
+
+                }
+            }
+            else if (value > root.item){
+                root.bf-=1;
+                root.rightnode = insertAVLTree(root.rightnode,value);
+            }
+        }
+        return root;
+    }
+    public static void rebalanced(AVLTreeNode node, int insertedKey) {
+        balancedFactor = getBalancedFactor(node);
+        if (balancedFactor > 1 && insertedKey < node.key)
+            return rightRotation(node);     //case 1
+        if (balancedFactor < -1 && insertedKey > node.key)
+            return leftRotation(node);      //case 4
+
+        // ......
+        // miss other 2 possibilities
+
+        // if do not need rebalanced (all conditions cannot satisfied)
+        // then return the current node
+        return node;
+    }
+    //中序遍历
+    public static void midTraverse(AVLTreeNode root){
+        if(root!=null){
+            midTraverse(root.leftnode);
+            System.out.printf("%d\t",root.bf);
+//            System.out.printf("%d\t",root.item);
+            midTraverse(root.rightnode);
+        }
     }
 }
