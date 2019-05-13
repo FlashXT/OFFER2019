13110eb4076a8508f77f19d7d401098c473887ab
turboMan <turboman2017@qq.com>
2019-05-13 21:53:46 +0800

    TencentOffer_Day12,2019.5.13,turboMan,Fighting!

---
+++ src/TencentOffer/CH5/Problem32_NumberOf1Between1andN/NumberOf1Between1andN2.java
@@ -0,0 +1,33 @@
+package TencentOffer.CH5.Problem32_NumberOf1Between1andN;
+
+/*****************************************************************
+ * @Author:FlashXT;
+ * @Date: 2019/5/13 18:48
+ * @Version 1.0
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ *****************************************************************/
+public class NumberOf1Between1andN2 {
+    public static void main(String [] args){
+
+        System.out.println(Numberof1(534));
+    }
+
+    public static int Numberof1(int n){
+        int weight = 0;
+        int base = 1;
+        int round = n;
+        int res = 0;
+        while( round > 0){
+
+            weight = round%10;
+            round = round/10;
+            res += round *base;
+            if(weight == 1)
+                res+=(n%base)+1;
+            else if(weight >1)
+                res +=base;
+            base*=10;
+        }
+        return res;
+    }
+}
