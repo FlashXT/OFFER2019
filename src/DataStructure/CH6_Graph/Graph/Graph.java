48e31e9d075498069e2bdc258db4643ae826ec0e
turboMan <turboman2017@qq.com>
2019-05-31 12:31:32 +0800

    Algorithm_Day2,2019.5.31,turboMan,Fighting!

--- src/DataStructure/CH6_Graph/Graph/Graph.java
+++ src/DataStructure/CH6_Graph/Graph/Graph.java
@@ -1,36 +1,38 @@
 package DataStructure.CH6_Graph.Graph;
 
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class Graph {
-    public HashMap<Integer,Node> nodes;
-    public HashSet<Edge> edges;
-    public Graph(){
-        this.nodes = new HashMap<>();
-        this.edges = new HashSet<>();
-    }
-    public Graph(Integer[][]matrix){
-        this.nodes = new HashMap<>();
-        this.edges = new HashSet<>();
 
-        for(int i =0;i < matrix.length;i++){
-            Integer weight  = matrix[i][0];
-            Integer from    = matrix[i][1];
-            Integer to      = matrix[i][2];
-            if(!this.nodes.containsKey(from))
-                this.nodes.put(from,new Node(from));
-            if(!this.nodes.containsKey(to))
-                this.nodes.put(to,new Node(to));
-            Node fromNode = this.nodes.get(from);
-            Node toNode = this.nodes.get(to);
-            Edge newEdge = new Edge(weight,fromNode,toNode);
-            fromNode.nexts.add(toNode);
-            fromNode.egdes.add(newEdge);
-            fromNode.outdegree++;
-            toNode.indegree++;
-            this.edges.add(newEdge);
+    int nodenum;
+    int edgenum;
+    boolean directed;
+    double [][] adjMatrix ;
 
+    public Graph(double [][] adjmatrix,boolean directed){
+        this.nodenum = adjmatrix.length;
+        this.adjMatrix = adjmatrix;
+        this.directed = directed;
+        this.edgenum = 0;
+        for(int i =0; i < adjmatrix.length;i++){
+            for(int j = 0; j < adjmatrix[i].length;j++){
+                if(adjmatrix[i][j]!=0)
+                    this.edgenum++;
+            }
         }
     }
+    public void addedge(int n1,int n2,double weight){
+        if( n1 >=0 && n1 < this.nodenum && n2 >=0 && n2 < this.nodenum){
+            if(this.adjMatrix[n1][n2] == 0)
+                this.edgenum++;
+            this.adjMatrix[n1][n2] = weight;
+            if(!this.directed)
+                this.adjMatrix[n2][n1] = weight;
+
+            return ;
+        }
+        throw  new ArrayIndexOutOfBoundsException("Node is not exist");
+
+    }
+
+
 }
