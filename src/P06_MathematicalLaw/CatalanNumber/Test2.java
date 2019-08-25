ca8f3af22361e8dc9d04dfaba163c38a6e6f396e
turboMan <turboman2017@qq.com>
2019-08-25 23:04:09 +0800

    ByteDance Test,2018.8.25,turboman.

---
+++ src/P06_MathematicalLaw/CatalanNumber/Test2.java
@@ -0,0 +1,10 @@
+package P06_MathematicalLaw.CatalanNumber;
+
+/*****************************************************************
+ * @Author:FlashXT;
+ * @Date: 2019/8/25 22:57
+ * @Version 1.0
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ *****************************************************************/
+public class Test2 {
+}
