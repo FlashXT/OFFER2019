f468b47b7d8edd03bc7fa6adb7ee3874efda7c27
turboMan <turboman2017@qq.com>
2019-08-31 22:52:47 +0800

    MeiTuan Test,2018.8.31,turboman.

---
+++ src/P05_CompanyCodeExam/MeiTuan/Problem2.java
@@ -0,0 +1,10 @@
+package P05_CompanyCodeExam.MeiTuan;
+
+/*****************************************************************
+ * @Author:FlashXT;
+ * @Date: 2019/8/31 20:04
+ * @Version 1.0
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ *****************************************************************/
+public class Problem2 {
+}
