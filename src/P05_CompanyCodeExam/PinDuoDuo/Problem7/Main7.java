666c1b8d9f4f9e1ebcbbd36c30989bb939505a52
FlashXT <976997551@qq.com>
2019-09-01 22:15:24 +0800

    Tencent Test,2018.9.1,turboman.

---
+++ src/P05_CompanyCodeExam/PinDuoDuo/Problem7/Main7.java
@@ -0,0 +1,10 @@
+package P05_CompanyCodeExam.PinDuoDuo.Problem7;
+
+/*****************************************************************
+ * @Author:FlashXT;
+ * @Date:2019/9/1,19:53
+ * @Version 1.0
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ *****************************************************************/
+public class Main7 {
+}
---
+++ src/P05_CompanyCodeExam/Tencent/Main1.java
@@ -0,0 +1,42 @@
+package P05_CompanyCodeExam.Tencent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*****************************************************************
+ * @Author:FlashXT;
+ * @Date:2019/9/1,19:54
+ * @Version 1.0
+ * CopyRight © 2018-2020,FlashXT & turboMan . All Right Reserved.
+ *****************************************************************/
+public class Main1 {
+    public static  void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        for(int q = 0;q < m;q++){
+            int n = scan.nextInt();
+            int [] arr = new int[n];
+            for(int i= 0; i< n;i++)
+                arr[i] = scan.nextInt();
+            del(arr);
+        }
+    }
+    public static void del(int [] arr){
+        if(arr.length%2 ==1){
+            System.out.println("NO");
+            return ;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i< arr.length;i++)
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        int max = Integer.MIN_VALUE;
+        for(Integer key:map.keySet()){
+            max = max < map.get(key)?map.get(key):max;
+        }
+        if(max > arr.length/2)
+            System.out.println("NO");
+        else
+            System.out.println("YES");
+    }
+}
